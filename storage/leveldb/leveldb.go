a8aeb432647179a05d8227e9a2e1cce639c213d1
Carl <1254503980@qq.com>
2019-09-07 14:08:27 +0800

    add leveldb stoage

---
+++ storage/leveldb/leveldb.go
@@ -0,0 +1,84 @@
+package leveldb
+
+import (
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/iterator"
+	"github.com/syndtr/goleveldb/leveldb/util"
+)
+
+type LevelDBStorage struct {
+	DB *leveldb.DB
+}
+
+func Open(path string) (*LevelDBStorage, error) {
+	leveldb, err := leveldb.OpenFile(path, nil)
+	if err != nil {
+		return &LevelDBStorage{}, err
+	}
+	return &LevelDBStorage{DB:leveldb}, nil
+}
+
+func (s *LevelDBStorage) Close() error {
+	return s.DB.Close()
+}
+
+func (s *LevelDBStorage) Get(key string) ([]byte, error)  {
+	return s.DB.Get([]byte(key), nil)
+}
+
+func (s *LevelDBStorage) Set(key string, value []byte) error {
+	return s.DB.Put([]byte(key), value,nil)
+}
+
+func iterateData(iter iterator.Iterator)  (data [][]byte, err error) {
+	for iter.Next() {
+		value := make([]byte, len(iter.Value()))
+		copy(value, iter.Value())
+		data = append(data,value)
+	}
+	err = iter.Error()
+	return
+}
+
+func (s *LevelDBStorage) Range(start, end string)([][]byte, error) {
+	iter := s.DB.NewIterator(&util.Range{Start:[]byte(start), Limit:[]byte(end)}, nil)
+	defer iter.Release()
+	return iterateData(iter)
+}
+
+func (s *LevelDBStorage) GetWithPrefix(prefix string) ([][]byte, error) {
+	iter := s.DB.NewIterator(util.BytesPrefix([]byte(prefix)), nil)
+	defer iter.Release()
+	return iterateData(iter)
+}
+
+func (s *LevelDBStorage) NewBatch() *Batch {
+	return &Batch{
+		db:    s,
+		batch: new(leveldb.Batch),
+	}
+}
+
+type Batch struct {
+	db *LevelDBStorage
+	batch *leveldb.Batch
+}
+
+func (b *Batch) Set(key string, value []byte) *Batch {
+	b.batch.Put([]byte(key), value)
+	return b
+}
+
+func (b *Batch) Delete(key string) *Batch {
+	b.batch.Delete([]byte(key))
+	return b
+}
+
+func (b *Batch) Len() int {
+	b.batch.Reset()
+	return b.batch.Len()
+}
+
+func (b *Batch) Commit() error {
+	return b.db.DB.Write(b.batch, nil)
+}
